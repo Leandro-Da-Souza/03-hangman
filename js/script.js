e69db74784d22df274b1a2c93582e91258b1ab07
Leandro Da Souza <leandro.dasouza@chasacademy.se>
2019-11-07 11:28:21 +0100

    init all global variables and create start game function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 // Globala variabler
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+const wordList = [
+    'Daughters',
+    'Neurosis',
+    'Swans',
+    'King Gizzard',
+    'Sunn 0)))',
+    'Tool',
+    'Radiohead',
+    'The Mars Volta',
+    'Electric Wizard'
+]; // Array: med spelets alla ord
 
-let guesses = 0;     // Number: håller antalet gissningar som gjorts
-let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+let selectedWord = wordList[Math.floor(Math.random() * wordList.length)]; // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+console.log(selectedWord);
 
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let guesses = 0; // Number: håller antalet gissningar som gjorts
+let hangmanImg = `../images/h${guesses}.png`; // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+
+let msgHolderEl = document.querySelector('#message'); // DOM-nod: Ger meddelande när spelet är över
+
+let startGameBtnEl = document.querySelector('#startGameBtn'); // DOM-nod: knappen som du startar spelet med
+
+let letterButtonEls = Array.from(
+    document.querySelectorAll('#letterButtons > li')
+); // Array av DOM-noder: Knapparna för bokstäverna
+
+let letterBoxEls = document.querySelector('#letterBoxes > ul'); // Array av DOM-noder: Rutorna där bokstäverna ska stå
+console.log(letterBoxEls);
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+startGameBtnEl.addEventListener('click', startGame);
+function startGame() {}
+
 // Funktion som slumpar fram ett ord
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
